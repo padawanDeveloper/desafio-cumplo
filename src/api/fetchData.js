fccbb62628ef3012689545575db7f5d7a12a3ed6
Esteban Ortiz <ematiasortiz@gmail.com>
2020-05-29 20:28:27 -0400

    feat(api): fetch SBIF api

---
+++ src/api/fetchData.js
@@ -0,0 +1,12 @@
+import axios from './axios';
+const API_KEY = '9c84db4d447c80c74961a72245371245cb7ac15f';
+
+const fetchDollarHistory = query =>
+  axios.get(query, {
+    params: {
+      apikey: API_KEY,
+      formato: 'json',
+    },
+  });
+
+export {fetchDollarHistory};
