9cd288a40d38385d3960b64ce51e7654f8af14e9
Esteban Ortiz <ematiasortiz@gmail.com>
2020-05-30 22:41:09 -0400

    feat(view): simple home view

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -2,25 +2,120 @@ import React, {useEffect, useState} from 'react';
 import {connect} from 'react-redux';
 import {RangePicker} from '../../components/pickers';
 import {fetchDollar} from '../../state/actions';
-import {Spinner} from '../../components/utils';
+import LineChart from '../../components/charts/LineChart';
+import {DollarStatics} from '../../components/cards';
+import {HomeLayout} from '../../layouts';
 
+const now = new Date();
 function HomeView(props) {
-  const [dates, setDates] = useState({startDate: null, endDate: null});
+  const [startDate, setStartDate] = useState(
+    new Date(now).setDate(now.getDate() - 1)
+  );
+  const [endDate, setEndDate] = useState(new Date());
+  const [fetch, setFetch] = useState(false);
+
   useEffect(() => {
-    props.fetchDollar();
-  }, []);
+    const fetchHistory = () => {
+      const dates = {
+        startDate,
+        endDate,
+      };
+      props.fetchDollar(dates).then(() => setFetch(false));
+    };
+    if (fetch) {
+      fetchHistory();
+    }
+  }, [fetch]);
 
-  if (props.state.isFetching) return <Spinner />;
+  return (
+    <HomeLayout>
+      <div
+        style={{
+          marginTop: 60,
+        }}
+      >
+        <DollarStatics
+          data={[
+            {field: 'Valor Promedio', value: 750},
+            {field: 'Valor Mínimo', value: 70},
+            {field: 'Valor Máximo', value: 800},
+          ]}
+          title="Dolar hoy"
+          subTitle="Valor observado el día 30 de Mayo 2020"
+          loading={props.state.isFetching}
+        />
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: 5,
+            backgroundColor: 'white',
+            flexDirection: 'wrap',
+            marginTop: 10,
+          }}
+        >
+          <div
+            style={{
+              margin: 10,
+              display: 'flex',
+              width: 500,
+              justifyContent: 'space-around',
+              alignItems: 'flex-end',
+            }}
+          >
+            <p style={{margin: 0}}>Buscar historial:</p>
+            <RangePicker
+              startDate={startDate}
+              endDate={endDate}
+              setStartDate={setStartDate}
+              setEndDate={setEndDate}
+              now={now}
+            />
+            <div>
+              <button onClick={() => setFetch(true)}>Buscar</button>
+            </div>
+          </div>
+        </div>
+        {props.state.data && props.state.data.length > 0 && (
+          <div
+            style={{
+              marginTop: 10,
+              display: 'flex',
+              flexWrap: 'wrap',
+              justifyContent: 'space-between',
+            }}
+          >
+            <div style={{width: 500, marginBottom: 10}}>
+              <DollarStatics
+                data={[
+                  {field: 'Promedio', value: 750},
+                  {field: 'Precio mínimo', value: 70},
+                  {field: 'Precio máximo', value: 800},
+                ]}
+                title="Dolar hoy"
+                subTitle="Valor observado el día 30 de Mayo 2020"
+                loading={props.state.isFetching}
+              />
+            </div>
 
-  return <RangePicker setDates={setDates} />;
+            <LineChart
+              data={props.state.data}
+              loading={props.state.isFetching}
+            />
+          </div>
+        )}
+      </div>
+    </HomeLayout>
+  );
 }
 
 const mapStateToProps = state => ({
-  state: state.dollarReducer,
+  state: state.dollar,
 });
 
 const mapDispatchToProps = dispatch => ({
-  fetchDollar: () => dispatch(fetchDollar()),
+  fetchDollar: dates => dispatch(fetchDollar(dates)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomeView);
