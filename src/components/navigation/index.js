e4b10d1be99cc281dcab9651bf4400e1777419bf
Esteban Ortiz <ematiasortiz@gmail.com>
2020-05-30 22:38:40 -0400

    feat(component):added navbar

---
+++ src/components/navigation/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default () => (
+  <div
+    style={{
+      height: 50,
+      width: '100%',
+      backgroundColor: '#3A454F',
+      position: 'fixed',
+      zIndex: 9,
+    }}
+  ></div>
+);
