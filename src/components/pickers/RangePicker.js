503816985bdd023248e42f2a0d6426a2f4c2fb98
Esteban Ortiz <ematiasortiz@gmail.com>
2020-05-31 20:25:51 -0400

    ref(picker): range picker

--- src/components/pickers/RangePicker.js
+++ src/components/pickers/RangePicker.js
@@ -3,42 +3,26 @@ import DatePicker from './DatePicker';
 
 const RangePicker = ({startDate, setStartDate, endDate, setEndDate, now}) => (
   <>
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        textAlign: 'center',
-      }}
-    >
-      <p>Inicio</p>
-      <DatePicker
-        selected={startDate}
-        onChange={setStartDate}
-        selectsStart
-        startDate={startDate}
-        endDate={endDate}
-        maxDate={new Date(endDate).setDate(new Date(endDate).getDate() - 1)}
-      />
-    </div>
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        textAlign: 'center',
-      }}
-    >
-      <p>Fin</p>
-      <DatePicker
-        selected={endDate}
-        onChange={setEndDate}
-        selectsEnd
-        startDate={startDate}
-        endDate={endDate}
-        minDate={new Date(startDate).setDate(new Date(startDate).getDate() + 1)}
-        maxDate={now}
-      />
-    </div>
+    <label>Inicio</label>
+    <DatePicker
+      selected={startDate}
+      onChange={setStartDate}
+      selectsStart
+      startDate={startDate}
+      endDate={endDate}
+      maxDate={new Date(endDate).setDate(new Date(endDate).getDate() - 1)}
+    />
+
+    <label>Fin</label>
+    <DatePicker
+      selected={endDate}
+      onChange={setEndDate}
+      selectsEnd
+      startDate={startDate}
+      endDate={endDate}
+      minDate={new Date(startDate).setDate(new Date(startDate).getDate() + 1)}
+      maxDate={now}
+    />
   </>
 );
 
